f7af154b4ff515b1e67889ec3dabf6b7d49c8c85
terentev.a.a <terentev.a.a@recloud.tech>
2024-11-02 19:58:43 +0300

    Add .gitignore and initial setup for Cassandra EF Core integration
    Introduced a .gitignore file for Visual Studio and other environments. Created initial project files for Cassandra.EntityFrameworkCore including a solution file, necessary extensions, and helper classes to support connection configuration and validation.

---
+++ src/Cassandra.EntityFrameworkCore/Extensions/CassandraContextOptionsExtensions.cs
@@ -0,0 +1,26 @@
+﻿using Cassandra.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+
+namespace Cassandra.EntityFrameworkCore.Extensions;
+
+public static class CassandraContextOptionsExtensions
+{
+    public static DbContextOptionsBuilder UseCassandraDatabase(
+        this DbContextOptionsBuilder optionsBuilder,
+        string connectionString,
+        string databaseName)
+    {
+        ArgumentNullException.ThrowIfNull(optionsBuilder);
+        ArgumentNullException.ThrowIfNull(connectionString);
+        
+        var extension = (optionsBuilder.Options.FindExtension<CassandraOptionsExtension>()
+                         ?? new CassandraOptionsExtension())
+            .WithConnectionString(connectionString)
+            .WithDatabaseName(databaseName);
+        
+        ((IDbContextOptionsBuilderInfrastructure)optionsBuilder).AddOrUpdateExtension(extension);
+        
+        return optionsBuilder;
+    }
+}
---
+++ src/Cassandra.EntityFrameworkCore/Infrastructure/CassandraOptionsExtension.cs
@@ -0,0 +1,63 @@
+﻿using Cassandra.EntityFrameworkCore.Helpers;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Cassandra.EntityFrameworkCore.Infrastructure;
+
+public class CassandraOptionsExtension : IDbContextOptionsExtension
+{
+    const string MultipleConnectionConfigSpecifiedException =
+        "Both ConnectionString and CassandraClient were specified. Specify only one set of connection details.";
+    
+    private string? _connectionString;
+    private string? _databaseName;
+    
+    public CassandraOptionsExtension()
+    {
+    }
+
+    /// <summary>
+    /// Creates a <see cref="MongoOptionsExtension"/> by copying from an existing instance.
+    /// </summary>
+    protected CassandraOptionsExtension(CassandraOptionsExtension copyFrom)
+    {
+        _connectionString = copyFrom._connectionString;
+    }
+
+    public DbContextOptionsExtensionInfo Info { get; }
+    
+    public void ApplyServices(IServiceCollection services)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Validate(IDbContextOptions options)
+    {
+        throw new NotImplementedException();
+    }
+    
+    public virtual CassandraOptionsExtension WithConnectionString(string connectionString)
+    {
+        ArgumentNullException.ThrowIfNull(connectionString);
+
+        if (_cassandraClient != null)
+        {
+            throw new InvalidOperationException(MultipleConnectionConfigSpecifiedException);
+        }
+
+        var clone = Clone();
+        clone._connectionString = connectionString;
+        return clone;
+    }
+    
+    public virtual CassandraOptionsExtension WithDatabaseName(string databaseName)
+    {
+        databaseName.ThrowArgumentExceptionIfNullOrEmpty();
+
+        var clone = Clone();
+        clone._databaseName = databaseName;
+        return clone;
+    }
+
+    protected virtual CassandraOptionsExtension Clone() => new(this);
+}
